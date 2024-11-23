0fe148172a4bab0a1cb47212b2d3ea14375f768d
brkcinar <brkcinar@hotmail.com.tr>
2024-11-23 22:37:36 +0100

    arka plan düzeltme

--- src/components/ProposalProcess.vue
+++ src/components/ProposalProcess.vue
@@ -16,4 +16,56 @@ const props = defineProps({
 const total = computed(() => props.pool?.bonded_tokens);
 const format = useFormatter();
 const yes = computed(() =>
-  format.calculatePercent(props.tally
+  format.calculatePercent(props.tally?.yes, total.value)
+);
+const no = computed(() =>
+  format.calculatePercent(props.tally?.no, total.value)
+);
+const abstain = computed(() =>
+  format.calculatePercent(props.tally?.abstain, total.value)
+);
+const veto = computed(() =>
+  format.calculatePercent(props.tally?.no_with_veto, total.value)
+);
+</script>
+
+<template>
+  <div class="progress rounded-[3px] h-6 text-xs flex items-center">
+    <div
+      class="h-6 bg-yes flex items-center pl-2 text-white overflow-hidden"
+      :style="`width: ${yes}`"
+      :title="yes"
+    >
+      {{ yes }}
+    </div>
+    <div
+      class="h-6 bg-no flex items-center text-white overflow-hidden"
+      :style="`width: ${no}`"
+      :title="no"
+    >
+      {{ no }}
+    </div>
+    <div
+      class="h-6 bg-[#B71C1C] flex items-center text-white overflow-hidden"
+      :style="`width: ${veto}`"
+      :title="veto"
+    >
+      {{ veto }}
+    </div>
+    <div
+      class="h-6 bg-secondary flex items-center text-white overflow-hidden"
+      :style="`width: ${abstain}`"
+      :title="abstain"
+    >
+      {{ abstain }}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.progress {
+  overflow: hidden;
+  background-color: #000000; /* Arka plan siyah */
+  border: 2px solid #d3d3d3; /* Kenarlar açık gri */
+}
+</style>
